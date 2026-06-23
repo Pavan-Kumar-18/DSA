4dccff9a5aa364570cccfc50acfffefe9792c959
Pavan-Kumar-18 <rpavankcr@gmail.com>
2026-06-23 17:35:16 +0530

    added new files

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+
+const app = express()
+const jobs = []
+
+app.post("/job",async(req, res)=>{
+
+    try{
+      const{title, company, salary} = req.body
+      if(!title || !company || !salary){
+        return res.status(400).json({message:"Please Provide all valid details"})
+      }
+        
+      jobs.push({title, company, salary})
+      
+      return res.status(200).json({message:"Successfully created jobs"})
+    }catch(err){
+        console.log(err)
+    }
+
+})
+
+
+app.listen(5000, ()=>{
+    console.log("Connected to backend")
+})
