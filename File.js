735f9ed09d042df0de217c74f86d9209d630220d
Pavan-Kumar-18 <rpavankcr@gmail.com>
2026-06-23 16:43:30 +0530

    Added function

--- File.js
+++ File.js
@@ -2,20 +2,20 @@
 
 // 1. 2 pointers 
 
-const arr = [2,4,6,7,9]
-const target = 9
+// const arr = [2,4,6,7,9]
+// const target = 9
 
-function twoPointers(arr, target){
-    let map = new Map()
-    for(let i=0; i<arr.length; i++){
-        const completed = target - arr[i]
-        if(map.has(completed)){
-            return [map.get(completed), i]
-        }
-        map.set(arr[i], i)
-    }
+// function twoPointers(arr, target){
+//     let map = new Map()
+//     for(let i=0; i<arr.length; i++){
+//         const completed = target - arr[i]
+//         if(map.has(completed)){
+//             return [map.get(completed), i]
+//         }
+//         map.set(arr[i], i)
+//     }
 
-}
-console.log(twoPointers(arr, target))
+// }
+// console.log(twoPointers(arr, target))
 
 //----------------------------------------------------------------------------------------------------
